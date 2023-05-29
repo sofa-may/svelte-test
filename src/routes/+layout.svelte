00e7a1e254164c119354a1e9960c5b8380be923a
Sofa <maysova.sofa08@mail.ru>
2023-05-29 15:49:10 +0300

    ddd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,35 @@
-
+<script>
+    const nav = [
+        {
+            title: "Обо мне",
+            href: "/about",
+            isShow: true,
+        },
+        {
+            title: "Блог",
+            href: "/blog",
+            isShow: true,
+        },
+        {
+            title: "Портфолио",
+            href: "/project",
+            isShow: true,
+        },
+        {
+            title: "Контакты",
+            href: "/contact",
+            isShow: true,
+        },
+    ];
+</script>
 
 <nav>
     <div class="nav-container">
         <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
         <div class="nav-links">
-            {#if link.isShow}
-            <a href={link.href} class="link">{link.title}</a>
-            {/if}
-            
+            {#each nav as link}
+                <a href={link.href} class="link">{link.title}</a>
+            {/each}
         </div>
     </div>
 </nav>
@@ -16,36 +38,7 @@
     <!-- Pages will be injected below -->
     <slot />
 </div>
-<script>
-    const nav = [
-  {
-    title: 'Обо мне',
-    href: '/about',
-    isShow: true,
-  },
-  {
-    title: 'Блог',
-    href: '/blog',
-    isShow: true,
-  },
-  {
-    title: 'Портфолио',
-    href: '/project',
-    isShow: true,
-  },
-  {
-    title: 'Контакты',
-    href: '/contact',
-    isShow: true,
-  },
-  {
-    title: 'temp',
-    href: '/temp',
-    isShow: false,
-  },
-];
 
-</script>
 <style>
     .container {
         max-width: 1400px;
