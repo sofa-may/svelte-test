827fadd6877ec47685e6227caa2cae88730fdcb3
Sofa <maysova.sofa08@mail.ru>
2023-05-29 16:20:47 +0300

    ofbipojfbhj

--- src/routes/blog/routes.js
+++ src/routes/blog/routes.js
@@ -1,8 +1,13 @@
 export const routes = [
     {
-      id: 1,
-      title: 'My First Blog Post',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate',
+        id: 1,
+        title: 'My First Blog Post',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate',
     },
-  ];
-  
+
+    {
+        "id": 2,
+        "title": "My Second Blog Post",
+        "body": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate"
+    }
+];
