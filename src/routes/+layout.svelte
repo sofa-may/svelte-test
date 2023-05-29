091e9d481c476da191b383886f923275543316b4
Sofa <maysova.sofa08@mail.ru>
2023-05-29 15:13:12 +0300

    74589

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,47 @@
+
+
 <nav>
     <div class="nav-container">
-      <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
-      <div class="nav-links">
-        <a href="/about" class="link">Обо мне</a>
-        <a href="/blog" class="link">Блог</a>
-        <a href="/projects" class="link">Портфолио</a>
-        <a href="/contact" class="link">Контакты</a>
-      </div>
+        <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
+        <div class="nav-links">
+            {#each nav as link}
+                <a href={link.href} class="link">{link.title}</a>
+            {/each}
+        </div>
     </div>
-  </nav>
-  
-  <div class="container">
+</nav>
+
+<div class="container">
     <!-- Pages will be injected below -->
-    <slot></slot>
-  </div>
-  
-  <style>
+    <slot />
+</div>
+<script>
+    const nav = [
+        {
+            title: "Обо мне",
+            href: "/about",
+        },
+        {
+            title: "Блог",
+            href: "/blog",
+        },
+        {
+            title: "Портфолио",
+            href: "/project",
+        },
+        {
+            title: "Контакты",
+            href: "/contact",
+        },
+        {
+            title: "temp",
+            href: "/temp",
+        },
+    ];
+</script>
+<style>
     .container {
-      max-width: 1400px;
-      margin: 50px auto;
+        max-width: 1400px;
+        margin: 50px auto;
     }
-  </style>
-  
+</style>
